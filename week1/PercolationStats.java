539fea23f7b6d9939c631eb498eeec5b355018d9
rlee <rlee@rallydev.com>
2013-03-26 13:26:07 -0600

    removed print statements from stats which hosed memory

--- week1/PercolationStats.java
+++ week1/PercolationStats.java
@@ -1,8 +1,8 @@
 public class PercolationStats {
     private Percolation percolation;
     private int N = -1;
+    private int T = -1;
     private int[] openSiteCount = null;
-    private double confidenceMultiplier = -1.0;
 
     // perform T independent computational experiments on an N-by-N grid
     public PercolationStats(int N, int T) {
@@ -11,12 +11,10 @@ public class PercolationStats {
                     + " be greater than 0.");
         }
         this.N = N;
+        this.T = T;
         openSiteCount = new int[T];
-        confidenceMultiplier = 1.96 / Math.sqrt(T);
         int openCount = 0;
-        System.out.print("Executing Test: ");
         for (int test = 0; test < T; test++) {
-            System.out.print(".");
             percolation = new Percolation(N);
             while (!percolation.percolates()) {
                 openNewSite();
@@ -25,7 +23,6 @@ public class PercolationStats {
             openSiteCount[test] = openCount;
             openCount = 0;
         }
-        System.out.println();
     }
 
     private void openNewSite() {
@@ -54,7 +51,7 @@ public class PercolationStats {
     // returns lower bound of the 95% confidence interval
     public double confidenceLo() {
         if (this.N > 1) {
-            return mean() - confidenceMultiplier * stddev();
+            return mean() - 1.96 * stddev() / Math.sqrt(T);
         }
         return Double.NaN;
     }
@@ -62,7 +59,7 @@ public class PercolationStats {
     // returns upper bound of the 95% confidence interval
     public double confidenceHi() {
         if (this.N > 1) {
-            return mean() + confidenceMultiplier * stddev();
+            return mean() + 1.96 * stddev() / Math.sqrt(T);
         }
         return Double.NaN;
     }
